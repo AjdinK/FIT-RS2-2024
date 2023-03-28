0a7058793cd69cd56e1b428c5f8249906298c168
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-03-28 01:11:50 +0100

    v1

---
+++ eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services {
+    public class ProizvodiService : IProizvodiService {
+
+        public List<Proizvodi> ProizvodiList = new List<Proizvodi>() { new Proizvodi() { ID = 1 , Naziv = "Laptop" } ,
+
+                new Proizvodi() { ID = 2, Naziv = "Mis" }};
+
+        public IEnumerable<Proizvodi> Get() {
+
+            ProizvodiList.Add(new Proizvodi() { Naziv = "test" , ID = -1});
+            return ProizvodiList;
+
+
+        }
+
+        public Proizvodi GetByID(int id ) {
+
+            return ProizvodiList.FirstOrDefault(x => x.ID == id);
+
+        }
+    }
+}
---
+++ eProdaja/Controllers/ProizvodiController.cs
@@ -0,0 +1,34 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : ControllerBase{
+
+        private readonly IProizvodiService _proizvodiService;
+
+        public ProizvodiController(IProizvodiService proizvodiService) {
+            _proizvodiService = proizvodiService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Proizvodi> Get() {
+            return _proizvodiService.Get();
+         }
+
+
+        [HttpGet("{id}")]
+        public Proizvodi GetByID (int id) {
+
+            return _proizvodiService.GetByID(id);
+
+        }
+
+        [HttpGet ("{naziv},{sifra}")]
+        public IEnumerable<Proizvodi> Get(string naziv , string sifra) {
+            return _proizvodiService.Get();
+        }
+
+    }
+}

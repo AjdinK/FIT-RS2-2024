7c13dd34ec28162d9e10acb9d88fbcd8611fa44e
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-04-28 12:28:14 +0200

    v3

--- eProdaja.Model/SearchObjects/JediniceMjereSearchObject.cs
+++ eProdaja.Model/SearchObjects/JediniceMjereSearchObject.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace eProdaja.Model.SearchObjects {
-    public class JediniceMjereSearchObject {
+    public class JediniceMjereSearchObject : BaseSearchObject {
 
         public string Naziv { get; set; }
         public int? JediniceMjereId { get; set; }
 
-        public int? Page { get; set; }
-        public int? PageSize { get; set; }
-
     }
+
 }
--- eProdaja.Model/SearchObjects/ProizvodiSearchObject.cs
+++ eProdaja.Model/SearchObjects/ProizvodiSearchObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace eProdaja.Model.SearchObjects {
-    public class ProizvodiSearchObject {
+    public class ProizvodiSearchObject : BaseSearchObject {
 
         public string Naziv { get; set; }
         public string Sifra { get; set; }

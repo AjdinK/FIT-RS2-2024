6b01520dd4dcde32f5789599ef82d7bcda90a6cb
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-05-18 14:10:47 +0200

    v7

--- eProdaja.Model/Requests/KorisniciUpdateRequest.cs
+++ eProdaja.Model/Requests/KorisniciUpdateRequest.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace eProdaja.Model.Requests {
     public class KorisniciUpdateRequest {
+        [Required(AllowEmptyStrings = false)]
         public string Ime { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string Prezime { get; set; }
+
+        [EmailAddress()]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string Telefon { get; set; }
         public string Password { get; set; }
         public string PasswordPotvrda { get; set; }

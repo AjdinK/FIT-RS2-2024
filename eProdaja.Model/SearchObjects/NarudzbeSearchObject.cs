8e27a0ef466e410fb870d3d09ac8be8373b5273b
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-06-08 15:44:34 +0100

    v10

--- eProdaja.Model/SearchObjects/NarudzbeSearchObject.cs
+++ eProdaja.Model/SearchObjects/NarudzbeSearchObject.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace eProdaja.Model.SearchObjects {
-    public class NaruzbeSearchObject : BaseSearchObject {
+    public class NarudzbeSearchObject : BaseSearchObject {
     }
 }

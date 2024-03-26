1c5d8221979f8ebc88f1f6f213905480460b2a6b
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-03-26 22:43:15 +0100

    update

--- eProdaja/eProdaja.Model/Requests/KorisniciUpdateRequest.cs
+++ eProdaja/eProdaja.Model/Requests/KorisniciUpdateRequest.cs
@@ -8,12 +8,9 @@ namespace eProdaja.Model.Requests
     {
         public string Ime { get; set; } = null!;
         public string Prezime { get; set; } = null!;
-
         public string? Telefon { get; set; }
-
         public string? Lozinka { get; set; }
         public string? LozinkaPotvrda { get; set; }
-
         public bool? Status { get; set; }
         public string? Email { get; set; }
     }
